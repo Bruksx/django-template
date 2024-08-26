091ccf860e2611216a01af3cb71d5faea0436a7a
Bruksx <jenake8@gmail.com>
2024-08-26 07:26:59 +0100

    add ninjaapi

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .api import api
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin_gtc_8hfj5dncgodxcuchnmaujsopq274/', admin.site.urls),
+    path('api/', api.urls)
 ]
