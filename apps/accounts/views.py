74686513da14f566d79ed222eb9a4fc933b09471
louieee <uzochukwu.ohaegbu.197108@unn.edu.ng>
2024-09-16 19:05:37 +0100

    add set_by_uid method to manytomany manager

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -111,7 +111,7 @@ def complete_talent_profile2(request, data: talent_schemas.CompleteTalentProfile
             Education(**edu_data, talent=talent_user).save()
     additional_languages = request_data.pop("additional_languages", list())
     talent_user.update(**request_data)
-    talent_user.additional_languages.set(additional_languages)
+    talent_user.additional_languages.set_by_uid(additional_languages)
     return talent_user.user
 
 
@@ -129,11 +129,11 @@ def complete_talent_profile3(request, data: talent_schemas.CompleteTalentProfile
         general = skill_data.pop("general_skills")
         soft = skill_data.pop("soft_skills")
         TalentSkill.objects.create(talent=talent_user, **skill_data)
-        talent_user.talentskill.soft_skills.set(soft)
-        talent_user.talentskill.general_skills.set(general)
-        talent_user.talentskill.business_models.set(business_models)
-        talent_user.talentskill.frameworks.set(frameworks)
-        talent_user.talentskill.tools.set(tools)
+        talent_user.talentskill.soft_skills.set_by_uid(soft)
+        talent_user.talentskill.general_skills.set_by_uid(general)
+        talent_user.talentskill.business_models.set_by_uid(business_models)
+        talent_user.talentskill.frameworks.set_by_uid(frameworks)
+        talent_user.talentskill.tools.set_by_uid(tools)
     for experience in data.experience_history:
         experience_data = experience.__dict__
         experience_uid = experience_data.pop("uid", None)
@@ -153,7 +153,6 @@ def talent_profile(request):
 @router.patch("talent-profile", response=talent_schemas.UserSchema, auth=JWTAuth())
 def update_talent_profile(request, data: talent_schemas.UpdateTalentProfileSchema):
     talent_user = Talent.objects.filter(user=request.user).first()
-    if not talent_user:
     if not talent_user:
         raise HttpError(403, "Not allowed")
     talent_user.user.update(first_name=data.first_name,
