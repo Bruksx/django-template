194ff28e0048f447a0ef23c222daad032ab033b5
louieee <uzochukwu.ohaegbu.197108@unn.edu.ng>
2024-11-08 16:36:15 +0100

    update ws schema

--- helpers/websocket/utils.py
+++ helpers/websocket/utils.py
@@ -3,18 +3,18 @@ import json
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 
-from .schemas import ChatWebsocketSchema
+from .schemas import ChatWebsocketSchema, NotificationWebsocketSchema
 from ..decorators import test_env_decorator
 from ..loggers import Logger
 
 
 @test_env_decorator()
-def send_ws(channel:str, data: ChatWebsocketSchema|dict):
+def send_ws(channel:str, data: ChatWebsocketSchema|NotificationWebsocketSchema):
     try:
         channel_layer = get_channel_layer()
         async_to_sync(channel_layer.group_send)(
             channel,
-            {"type": "notify", "data": json.dumps(data)}
+            {"type": "notify", "data": json.dumps(data.__dict__)}
         )
     except Exception as e:
         Logger.error(dict(
