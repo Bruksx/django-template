66ae91b650e1a680bbd05f56b38b5ee37d9f294c
louieee <uzochukwu.ohaegbu.197108@unn.edu.ng>
2025-04-03 13:04:44 +0100

    update ws

--- apps/chats/consumers.py
+++ apps/chats/consumers.py
@@ -121,7 +121,6 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
                 return
         logging.critical("past validation")
-        await sync_to_async(async_task)(send_new_chat_notification, self.chat)
         message, data = await self.create_chat_message(body.get("body"),job_post, body.get("attachments"))
         await sync_to_async(message.handle_post_save)(notify=False)
         await sync_to_async(self.chat.refresh_from_db)()
