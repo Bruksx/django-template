34ba0a267f42685486fddc5102386d07562b79f8
louieee <uzochukwu.ohaegbu.197108@unn.edu.ng>
2025-04-10 11:41:29 +0100

    update job endpoint

--- apps/jobs/services.py
+++ apps/jobs/services.py
@@ -1,12 +1,16 @@
+from uuid import UUID
+
 from config import settings
 from django.conf import settings
 from django.db import transaction
+from django_q.tasks import async_task
 from helpers.utils import upload_to_s3, upload_to_server
 from ninja.errors import HttpError
+from typing_extensions import Optional
 
 from jobs.enums import PhaseType
-from jobs.models import JobApplication, Answer
-from jobs.schemas import ApplyToJobSchema
+from jobs.models import JobApplication, Answer, RequiredAttribute
+from jobs.schemas import ApplyToJobSchema, MutateRequiredAttributeSchema
 from notification.notifications import send_talents_job_matching_notification
 from settings.models import WorkFlowStage
 
@@ -65,4 +69,14 @@ def notify_business_on_matched_talents(job):
     for post in posts:
         talent_count = post.get_talents().count()
         send_talents_job_matching_notification(talent_count, post)
+
+
+def set_job_required_attributes(data:dict, job):
+    MutateRequiredAttributeSchema.validate_required_attribute(data, job)
+    required_attributes, _ = RequiredAttribute.objects.get_or_create(job=job)
+    required_attributes.skills.set(data.pop("skills"))
+    required_attributes.business_models.set(data.pop("business_models"))
+    required_attributes.update(**data)
+    async_task(notify_business_on_matched_talents, job=job)
+    return required_attributes
 
