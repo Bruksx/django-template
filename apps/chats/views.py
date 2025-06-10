614436b8fc7d50b61bc17f599590c0192b8b46a7
louieee <uzochukwu.ohaegbu.197108@unn.edu.ng>
2025-06-10 16:47:37 +0100

    fix

--- apps/chats/views.py
+++ apps/chats/views.py
@@ -3,7 +3,7 @@ from uuid import UUID
 
 from config.permissions import IsBusinessUser
 from django.db import transaction
-from django.db.models import Q, F, OrderBy
+from django.db.models import Q, F, OrderBy, Case, When, Value, IntegerField
 from monkeypatches.q_cluster import async_task
 from monkeypatches.response import Response
 from ninja import Router, UploadedFile, Form
@@ -30,7 +30,13 @@ ws_router = Router(tags=["Websocket"])
 @paginate(PageNumberPaginationExtra, page_size=50)
 def get_chats(request, search:str=""):
     user = request.user
-    queryset = Conversation.objects.filter(users__id=user.id).order_by(OrderBy(F("last_message_time"), descending=True, nulls_last=True))
+    queryset = Conversation.objects.filter(users__id=user.id).annotate(
+        null_order=Case(
+            When(last_message_time__isnull=True, then=Value(1)),
+            default=Value(0),
+            output_field=IntegerField()
+        )
+    ).order_by('null_order', '-last_message_time')
 
     if search:
         queryset = queryset.filter(Q(message__body__icontains=search)|
