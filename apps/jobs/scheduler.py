1594a1f3e500ccc06fce875039ebf4bb9d00b848
louieee <uzochukwu.ohaegbu.197108@unn.edu.ng>
2025-06-23 19:13:15 +0100

    schedule job downloader

--- apps/jobs/scheduler.py
+++ apps/jobs/scheduler.py
@@ -22,7 +22,12 @@ tasks = [
         },
         {
             "name": "Job Performance Notification - Friday 3 PM",
-            "func": "job_performance_notification_task",
+            "func": "jobs.tasks.job_performance_notification_task",
             "cron": "0 15 * * 5",  # Every Friday at 3:00 PM
         },
+        {
+        "name": "Lever JobPost Download - 1 AM",
+        "func": "jobs.tasks.fetch_job_posts_from_lever",
+        "cron": "0 1 * * *",  # Every day at 1:00 AM
+        }
     ]
