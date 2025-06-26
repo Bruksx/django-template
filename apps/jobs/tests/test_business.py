4ff963163d2f9a544d462b9dc189e4e2f9c9f6d4
louieee <uzochukwu.ohaegbu.197108@unn.edu.ng>
2025-06-26 17:44:24 +0100

    update jobpost update test

--- apps/jobs/tests/test_business.py
+++ apps/jobs/tests/test_business.py
@@ -831,9 +831,10 @@ class JobPostUpdateTest(TestCase):
 
         self.job_post.refresh_from_db()
 
-
+        self.assertEqual(self.test_data["status"], response.json()["status"])
+        self.assertNotEqual(self.job_post.uid, response.json()["uid"])
         self.assertEqual(self.job_post.benefits, self.test_data["benefits"])
-        self.assertEqual(self.job_post.status, self.test_data["status"])
+        self.assertEqual(self.job_post.status, "closed")
         self.assertEqual(self.job_post.annual_salary_currency, self.currency)
         self.assertEqual(self.job_post.annual_bonus_currency, self.currency)
         self.assertEqual(self.job_post.annual_salary_min, self.test_data["annual_salary_min"])
