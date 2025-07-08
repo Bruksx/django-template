1a0f7abf6d91c66a46a8121710c0c00e03c278f6
louieee <uzochukwu.ohaegbu.197108@unn.edu.ng>
2025-07-08 17:00:45 +0100

    fix workflow stage

--- apps/settings/models.py
+++ apps/settings/models.py
@@ -145,7 +145,7 @@ class WorkFlowStage(BaseModel):
 
 
     def can_be_deactivated(self):
-        return self.applicants().count() == 0
+        return self.applicants().count() == 0 or self.phase in [PhaseType.NEW.value, PhaseType.REJECTED.value, PhaseType.HIRED.value]
 
     def previous_stages(self):
         """
