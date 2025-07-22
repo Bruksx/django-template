11246d821f49ebbaf72572f344b4bc73c2ad96e5
louieee <uzochukwu.ohaegbu.197108@unn.edu.ng>
2025-07-23 00:09:57 +0100

    fix test

--- apps/chats/schemas.py
+++ apps/chats/schemas.py
@@ -10,19 +10,32 @@ from chats.models import Message, MessageAttachment, Conversation
 from jobs.models import JobPost
 from paginations import CustomPaginatedResponseSchema as PaginatedResponseSchema
 
+from apps.accounts.enums import UserType
+from ws_tester import business_user
+
 
 class ChatUserSchema(ModelSchema):
     photo_url:Optional[str] = None
-    talent:Optional[UUID] = None
-    business_user:Optional[UUID] = None
+    user_type_uid:Optional[UUID] = None
 
     class Meta:
         model = User
         fields = ("uid", "email", "first_name", "last_name", "type")
 
     @staticmethod
-    def resolve_talent_uid(obj):
-        return Talent.objects.filter(user=obj).first()
+    def resolve_user_type_uid(obj):
+        if obj.type == UserType.TALENT.value:
+            talent = Talent.objects.filter(user=obj).first()
+            if not talent:
+                return
+            return talent.uid
+        elif obj.type == UserType.BUSINESS.value:
+            business_user = BusinessUser.objects.filter(user=obj).first()
+            if not business_user:
+                return
+            return business_user.uid
+        return
+
 
     @staticmethod
     def resolve_business_user_uid(obj):
