b07aa24e1cd577270d729aa29222cbc211013fda
louieee <uzochukwu.ohaegbu.197108@unn.edu.ng>
2025-07-25 14:27:57 +0100

    update

--- apps/accounts/enums.py
+++ apps/accounts/enums.py
@@ -87,4 +87,5 @@ class CaseReasonType(BaseEnum):
     TECHNICAL_ISSUES = "Technical Issues"
     SYSTEM_HELP = "System Help"
     REPORT_BUSINESS = "Report a Business"
-    OTHERS = "Others"
+    REPORT_TALENT = "Report a Talent"
+    OTHERS = "Other"
