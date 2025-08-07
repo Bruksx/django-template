bd9374d65b6cc90fd4d30bedd995230f4b8a381b
louieee <uzochukwu.ohaegbu.197108@unn.edu.ng>
2025-08-07 19:26:29 +0100

    update admin job application view

--- apps/jobs/admin.py
+++ apps/jobs/admin.py
@@ -12,5 +12,17 @@ admin.site.register(Job)
 admin.site.register(BusinessModel)
 admin.site.register(ScreeningQuestion)
 admin.site.register(QuestionOption)
-admin.site.register(JobApplication)
+# admin.site.register(JobApplication)
 admin.site.register(AvailableDay)
+
+
+
+@admin.register(JobApplication)
+class JobApplicationAdmin(admin.ModelAdmin):
+    list_display = ('id','uid','applicant__user__fullname', 'job_post__job__role__name', 'stage__name')
+    search_fields = ('applicant__user__email', 'applicant__user__fullname', "uid")
+    list_filter = ('stage__phase', 'deleted_at','stage__created_by__business__name')
+
+    def get_queryset(self, request):
+        return JobApplication.global_objects.all()
+
