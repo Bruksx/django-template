8e27c898925ee70f334abd78b840027ffbc63175
louieee <uzochukwu.ohaegbu.197108@unn.edu.ng>
2025-08-07 21:25:31 +0100

    fix

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -9,7 +9,7 @@ from uuid import UUID
 from django.contrib.auth.hashers import check_password, make_password
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
-from django.db.models import Q, Count, F, Value, Avg, IntegerField, Exists, OuterRef, Sum, When, Case
+from django.db.models import Q, Count, F, Value, Avg, IntegerField, Exists, OuterRef, Sum, When, Case, FloatField
 from django.db.models.functions import Concat, Cast, Coalesce
 from django.db.models.signals import pre_save
 from django.utils import timezone
@@ -745,9 +745,9 @@ class Business(BaseModel):
                   filter=Q(talentapplicationstagetimeline__job_role_id=job_role_id,
                            talentapplicationstagetimeline__application__deleted_at__isnull=True)),
                  stage=F("name"))
-        graph = graph.annotate(avg_timeline=Case(
-            When(avg_timelines__isnull=True, then=float(0)), default=F("avg_timelines")))
-        return {"graph": graph.values("stage", "avg_timeline"), "days_to_hire": graph.aggregate(Avg("avg_timeline"))["avg_timeline__avg"] or 0}
+        graph = graph.annotate(avg_timeline=Cast(Case(
+            When(avg_timelines__isnull=True, then=float(0)), default=F("avg_timelines")),output_field=IntegerField()))
+        return {"graph": graph.values("stage", "avg_timeline"), "days_to_hire": int(graph.aggregate(Avg("avg_timeline"))["avg_timeline__avg"] or 0)}
 
 
     def time_to_hire_via_stage(self, start_date:date=None, end_date:date=None, role_id: UUID=None, client: str=None):
--- runner.py
+++ runner.py
@@ -1,60 +1,53 @@
 import os
 
 import django
-from django.db.models import Subquery, OuterRef
-from django.db.models.functions import FirstValue
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
-from jobs.models import JobApplication, JobPost
+from jobs.models import JobPost
 from jobs.queries import add_job_post_annotations
 
-from django.db.models import Count, Avg, Sum, F, Subquery, Q, OuterRef, JSONField, Exists
-from jobs.models import TalentApplicationStageTimeline, Job, JobApplication, JobInvite
-from settings.models import WorkFlowStage
-from jobs.enums import PhaseType
+from jobs.models import JobApplication
 
+# job = Job.objects.annotate(count=Count('jobpost__jobapplication')).order_by('-count').first()
+# job_post = job.jobpost_set.annotate(count=Count('jobapplication')).order_by('-count').first()
+#
+# application:JobApplication = job_post.jobapplication_set.filter(stage__phase=PhaseType.REJECTED.value).first()
+# business = job.created_by.business
+# job_role_id = job.role_id
+# stages = WorkFlowStage.objects.filter(created_by__business=business).order_by("phase_order", "order")
+# recruiter = application.recruiter
+# job.workflow_stage_data()
+#
+# job_post.workflow_stage_data()
+#
+# JobApplication.objects.get(id=99).update(stage=stages.filter(phase=PhaseType.HIRED.value).first())
+# JobApplication.objects.filter(id=99).update(recruiter=None)
+#
+# application_count = job_post.jobapplication_set.count()
+# query = JobApplication.objects.select_related("stage", "applicant", "applicant__user",
+#                                                      "applicant__country").annotate(invited=Exists(Subquery(JobInvite.objects.filter(
+#             job=OuterRef('job_post__job'), talent=OuterRef('applicant')
+#         )))).filter(job_post__uid=job_post.uid, stage__created_by__business=business,
+#                                              applicant__deleted_at__isnull=True)
+# print(application_count)
+# print()
+# print(job_post.workflow_stage_data())
+#
+# print(job_post.workflow_stage_data())
+#
+# print(query.filter(id=99).first())
+# print(query.count())
+# print(JobApplication.global_objects.filter(id=99).first())
 
-job = Job.objects.annotate(count=Count('jobpost__jobapplication')).order_by('-count').first()
-job_post = job.jobpost_set.annotate(count=Count('jobapplication')).order_by('-count').first()
+# for ap in JobApplication.global_objects.filter(job_post=job_post):
+#     ap.restore()
+# print([TimeToHireViaStages(**dt) for dt in (business.time_to_hire_via_stage())])
 
-application:JobApplication = job_post.jobapplication_set.filter(stage__phase=PhaseType.REJECTED.value).first()
-business = job.created_by.business
-job_role_id = job.role_id
-stages = WorkFlowStage.objects.filter(created_by__business=business).order_by("phase_order", "order")
-recruiter = application.recruiter
-job.workflow_stage_data()
-
-job_post.workflow_stage_data()
-
-JobApplication.objects.filter(id=99).update(stage=stages.filter(phase=PhaseType.NEW.value).first())
-JobApplication.objects.filter(id=99).update(recruiter=None)
-
-application_count = job_post.jobapplication_set.count()
-query = JobApplication.objects.select_related("stage", "applicant", "applicant__user",
-                                                     "applicant__country").annotate(invited=Exists(Subquery(JobInvite.objects.filter(
-            job=OuterRef('job_post__job'), talent=OuterRef('applicant')
-        )))).filter(job_post__uid=job_post.uid, stage__created_by__business=business,
-                                             applicant__deleted_at__isnull=True)
-print(application_count)
-print()
-print(job_post.workflow_stage_data())
-
-print(job_post.workflow_stage_data())
-
-print(query.filter(id=99).first())
-print(query.count())
-print(JobApplication.global_objects.filter(id=99).first())
-
-for ap in JobApplication.global_objects.filter(job_post=job_post):
-    ap.restore()
-
-
-# for ja in JobApplication.objects.iterator():
-#     jp = add_job_post_annotations(JobPost.objects.filter(id=ja.job_post_id), ja.applicant).first()
-#     if not jp:
-#         ja.update(match=0)
-#     else:
-#         print("computed_match_score", jp.computed_match_score)
-#         ja.update(match=int(jp.computed_match_score))
+for ja in JobApplication.objects.iterator():
+    jp = add_job_post_annotations(JobPost.objects.filter(id=ja.job_post_id), ja.applicant).first()
+    if not jp:
+        ja.update(match=0)
+    else:
+        print("computed_match_score", jp.computed_match_score)
+        ja.update(match=int(jp.computed_match_score))
