4015945daa71a896ab5e0897cd3ee5c8eb5ceb0c
louieee <uzochukwu.ohaegbu.197108@unn.edu.ng>
2025-10-03 14:18:05 +0100

    update

--- apps/core/migrations/0012_auto_20251002_1428.py
+++ apps/core/migrations/0012_auto_20251002_1428.py
@@ -9,7 +9,7 @@ def remove_duplicate_currency(apps, schema_editor):
         count = Currency.objects.filter(abbreviation__iexact=abbreviation).count()
         if count > 1:
             last_currency = Currency.objects.filter(abbreviation__iexact=abbreviation).order_by("id").first()
-            Currency.objects.filter(abbreviation__iexact=abbreviation).exclude(id=last_currency.id).hard_delete()
+            Currency.objects.filter(abbreviation__iexact=abbreviation).exclude(id=last_currency.id).delete()
 
 
 class Migration(migrations.Migration):
