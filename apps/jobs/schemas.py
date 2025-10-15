6636c88aae9ab3e329370b0b5171960e16e9f463
Bruksx <jenake8@gmail.com>
2025-10-15 12:38:56 +0100

    debug match score

--- apps/jobs/schemas.py
+++ apps/jobs/schemas.py
@@ -1001,6 +1001,8 @@ class MatchScoreSchema(Schema):
     requires_tech_requirements: bool
     missing_work_schedule: bool
     missing_required_business_model: bool
+    tools_platform_count: Optional[int] = 0
+    tools_platform_intercept_count: Optional[int] = 0
 
     class Meta:
         orm_mode = True
