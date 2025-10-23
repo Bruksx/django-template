b2004977e051e8791b852b843ee707fced0e1847
louieee <uzochukwu.ohaegbu.197108@unn.edu.ng>
2025-10-23 21:46:06 +0100

    fix template

--- helpers/email/jobs.py
+++ helpers/email/jobs.py
@@ -28,7 +28,7 @@ def send_shared_job_email(job_post, emails: List[str]=None, lang="en"):
 	context = {
 		'talent': "User",
 		'company_logo': job_post.job.logo_url(),
-		'job_link': f"{frontend_job_url}/{job_post.job.uid}",
+		'job_link': f"{frontend_job_url}/{job_post.uid}",
 		'job_title': job_post.job.get_title,
 		'company': job_post.job.hiring_company(),
 		'location': job_post.get_location(),
