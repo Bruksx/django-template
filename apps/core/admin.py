f6bd0faab0313604c376feeeaf0e82426d903f7d
Bruksx <jenake8@gmail.com>
2025-11-19 10:26:01 +0100

    add gtc settings to admin

--- apps/core/admin.py
+++ apps/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Currency
+from .models import Currency, GTCSettings
 
 # Register your models here.
 admin.site.register(Currency)
+admin.site.register(GTCSettings)
