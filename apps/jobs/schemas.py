a8d92612ceb7c7522d38dacaff52f014df3f9c9c
louieee <uzochukwu.ohaegbu.197108@unn.edu.ng>
2025-11-20 14:39:20 +0100

    update

--- apps/jobs/schemas.py
+++ apps/jobs/schemas.py
@@ -862,7 +862,7 @@ class JobPostFullDetailSchema(ModelSchema):
     
     @staticmethod
     def resolve_withdrawals(obj):
-        return obj.jobapplicationiwithdrawal_set.count()
+        return obj.jobapplicationwithdrawal_set.count()
 
     @staticmethod
     def resolve_workflow_data(obj, context):
