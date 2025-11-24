c57faf17affe0ec13230b59c5820cec9d8d0ef74
louieee <uzochukwu.ohaegbu.197108@unn.edu.ng>
2025-11-24 12:10:14 +0100

    update

--- apps/accounts/views/talent.py
+++ apps/accounts/views/talent.py
@@ -113,7 +113,7 @@ def talent_dashboard_chart(request):
     IsTalentUser.check(request)
     return request.user.talent.dashboard_charts()
 
-@router.patch("profile", auth=JWTAuth())
+@router.patch("profile", auth=JWTAuth(), response=talent_schemas.TalentUserListSchema)
 @transaction.atomic
 def update_talent_profile(request, data: PatchDict[talent_schemas.UpdateTalentProfileSchema2]):
     IsTalentUser.check(request)
@@ -227,8 +227,8 @@ def update_talent_profile(request, data: PatchDict[talent_schemas.UpdateTalentPr
                 if not talent_user.talentavailableday_set.filter(day=day).exists():
                     TalentAvailableDay.objects.create(**available_day, talent=talent_user)
     user.update(**user_data)
-    talent_user.update(**data)
-    return Response(status=200, data={"message": "Profile updated successfully"})
+    return talent_user.update(**data)
+    
 
 @router.patch("change-password", auth=JWTAuth())
 def change_talent_password(request, data: talent_schemas.TalentChangePasswordSchema):
