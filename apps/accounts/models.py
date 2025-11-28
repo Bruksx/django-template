83f5aae967f51e44c721417461e6b2689b9d75f7
louieee <uzochukwu.ohaegbu.197108@unn.edu.ng>
2025-11-28 13:32:05 +0100

    updates

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -1,3 +1,4 @@
+import logging
 import random
 import random
 import secrets
@@ -15,6 +16,7 @@ from core.models import BaseModel, State, City
 from dateutil.relativedelta import relativedelta
 from django.contrib.auth.hashers import check_password, make_password
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.core.exceptions import ValidationError
 from django.db import models
 from django.db.models import Q, Count, F, Value, Avg, IntegerField, Exists, OuterRef, Sum, When, Case
 from django.db.models.functions import Concat, Cast, Round
@@ -221,7 +223,7 @@ class Talent(BaseModel):
     viber_number = models.CharField(max_length=50, null=True)
     country = models.ForeignKey(Country, on_delete=models.SET_NULL, null=True)
     state = models.ForeignKey(State, on_delete=models.SET_NULL, null=True)
-    city = models.ForeignKey(City, on_delete=models.SET_NULL, null=True)
+    city = models.CharField(max_length=200, null=True)
     address = models.CharField(max_length=128, null=True)
     postal_code = models.CharField(max_length=20, null=True)
     employment_types = models.ManyToManyField('jobs.EmploymentType', blank=True)
@@ -257,7 +259,7 @@ class Talent(BaseModel):
         #     data.append(self.address)
 
         if self.city:
-            data.append(self.city.name)
+            data.append(self.city)
         if self.state:
             data.append(self.state.name)
         if self.country:
@@ -543,8 +545,43 @@ class Talent(BaseModel):
         self.cv = None
         self.save()
         self.delete()
-
-
+        
+    def is_profile_completed(self):
+        return bool(self.education_set.exists() and
+        self.experience_set.exists() and
+        bool(self.country) and
+        bool(self.state) and
+        bool(self.postal_code) and
+        bool(self.bio) and
+        bool(self.linkedin) and
+        bool(self.notice_period) and
+        self.skills.exists() and
+        bool(self.photo) and
+        bool(self.cv))
+    
+    def validate_profile_completed(self):
+        if not self.education_set.exists():
+            raise ValidationError("Education is required.")
+        if not self.experience_set.exists():
+            raise ValidationError("Experience is required.")
+        if not self.country:
+            raise ValidationError("Country is required.")
+        if not self.state:
+            raise ValidationError("State is required.")
+        if not self.postal_code:
+            raise ValidationError("Postal code is required.")
+        if not self.bio:
+            raise ValidationError("Bio is required.")
+        if not self.linkedin:
+            raise ValidationError("LinkedIn profile link is required.")
+        if not self.notice_period:
+            raise ValidationError("Notice period is required.")
+        if not self.skills.exists():
+            raise ValidationError("At least one skill is required.")
+        if not self.photo:
+            raise ValidationError("Profile photo is required.")
+        if not self.cv:
+            raise ValidationError("CV upload is required.")
 class BusinessIndustry(BaseModel):
     name = models.CharField(max_length=128)
 
--- apps/accounts/views/talent.py
+++ apps/accounts/views/talent.py
@@ -7,9 +7,9 @@ from accounts.models import Talent, TalentAvailableDay
 from accounts.models import User, VerificationCode, Education, Experience
 from accounts.schemas import common as common_schemas
 from accounts.schemas import talent as talent_schemas
+from django.db import transaction
 from auth.services import validate_login
 from auth.schema import LoginSchema
-from django.db import transaction
 from django.utils import timezone
 from ninja import Router, PatchDict, UploadedFile, File
 from ninja.errors import HttpError
@@ -115,7 +115,7 @@ def talent_dashboard_chart(request):
     IsTalentUser.check(request)
     return request.user.talent.dashboard_charts()
 
-@router.patch("profile", auth=JWTAuth())
+@router.patch("profile", auth=JWTAuth(), response=talent_schemas.TalentUserListSchema)
 @transaction.atomic
 def update_talent_profile(request, data: PatchDict[talent_schemas.UpdateTalentProfileSchema2]):
     IsTalentUser.check(request)
@@ -229,8 +229,8 @@ def update_talent_profile(request, data: PatchDict[talent_schemas.UpdateTalentPr
                 if not talent_user.talentavailableday_set.filter(day=day).exists():
                     TalentAvailableDay.objects.create(**available_day, talent=talent_user)
     user.update(**user_data)
-    talent_user.update(**data)
-    return Response(status=200, data={"message": "Profile updated successfully"})
+    return talent_user.update(**data)
+    
 
 @router.patch("change-password", auth=JWTAuth())
 def change_talent_password(request, data: talent_schemas.TalentChangePasswordSchema):
