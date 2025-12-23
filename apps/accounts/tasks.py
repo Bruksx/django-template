56410d3fda4af53b07c5fa82541091bb1f149378
Louis Ohaegbu <uzochukwu.ohaegbu.197108@unn.edu.ng>
2025-12-23 17:43:06 +0100

    fix :fix remind_incomplete_profiles_task function

--- apps/accounts/tasks.py
+++ apps/accounts/tasks.py
@@ -1,4 +1,4 @@
-from accounts.models import Talent
+from accounts.models import Talent, User
 from accounts.queries import add_profile_completion_annotation
 
 from helpers.email.accounts import send_incomplete_profile_reminder_email
@@ -12,9 +12,9 @@ def remind_incomplete_profiles_task(batch_size=200):
     talent_count = talent_ids.count()
     batches, remainder = divmod(talent_count, batch_size)
     for batch in range(batches):
-        emails = list(Talent.objects.filter(id__in=talent_ids[(batch * batch_size):((batch + 1) * batch_size)]).only("email").values_list("email", flat=True))
+        emails = list(User.objects.filter(talent_id__in=talent_ids[(batch * batch_size):((batch + 1) * batch_size)]).only("email").values_list("email", flat=True))
         send_incomplete_profile_reminder_email(emails=emails)
     if remainder > 0:
-        emails = list(Talent.objects.filter(id__in=talent_ids[-remainder:]).only(
+        emails = list(User.objects.filter(talent_id__in=talent_ids[-remainder:]).only(
             "email").values_list("email", flat=True))
         send_incomplete_profile_reminder_email(emails=emails)
