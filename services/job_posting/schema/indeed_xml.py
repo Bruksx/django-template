86efcd8e472668f8ed38356f0a739ecf19a829e1
Louis Ohaegbu <39104306+louieee@users.noreply.github.com>
2026-01-09 10:57:50 +0100

    Update job post URL format in indeed_xml.py

--- services/job_posting/schema/indeed_xml.py
+++ services/job_posting/schema/indeed_xml.py
@@ -20,7 +20,7 @@ from helpers.utils import alert_bug_via_email, html_to_text
 BASE_FRONTEND_URL = settings.FRONTEND_URL
 BASE_BACKEND_URL = settings.BACKEND_URL
 
-JOB_POST_URL = lambda job_post_uid: f"{BASE_FRONTEND_URL}job-details/{job_post_uid}"
+JOB_POST_URL = lambda job_post_uid: f"{BASE_FRONTEND_URL}jobs-listing/{job_post_uid}"
 
 SCREENING_QUESTIONS_URL = lambda job_uid: f"{BASE_BACKEND_URL}/business/jobs/{job_uid}/indeed/screener-questions"
 INDEED_EMAIL = settings.INDEED_EMAIL
