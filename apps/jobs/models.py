77c11d2eddd212279e57e8e6c46cd80065492dda
Louis Ohaegbu <uzochukwu.ohaegbu.197108@unn.edu.ng>
2026-01-15 19:16:07 +0100

    update

--- apps/jobs/models.py
+++ apps/jobs/models.py
@@ -1,19 +1,19 @@
 import string
 from functools import cached_property
 
+from accounts.enums import Days
+from accounts.models import Talent, TalentAvailableDay
+from core.enums import SalaryType
+from core.models import BaseModel, Language
 from django.db import models
 from django.db.models import F, Q, Count, IntegerField, When, Case, Value
 from django.db.models.functions import Coalesce, Now, Extract, Cast
 from django.db.models.signals import pre_save
 from django_softdelete.managers import SoftDeleteManager
-from timezone_field import TimeZoneField
-
-from accounts.enums import Days
-from accounts.models import Talent, TalentAvailableDay
-from core.enums import SalaryType
-from core.models import BaseModel, Language
 from jobs.managers import JobManager
 from settings.enums import PlaceHolderType
+from timezone_field import TimeZoneField
+
 from .db_functions import Epoch
 from .enums import WorkStructureEnum, LunchBreakEnum, QuestionTypeEnum, PhaseType, WithdrawalFeedbackType, \
     JobStatusType, ScreeningResultStatusType
@@ -320,7 +320,7 @@ class JobPost(BaseModel):
     edited_at = models.DateTimeField(null=True)
     
     promotion_code = models.CharField(max_length=200, null=True, blank=True)
-    linked_in_tags = models.JSONField(default=list)
+    linkedin_tags = models.JSONField(default=list)
 
 
     def get_tags(self):
