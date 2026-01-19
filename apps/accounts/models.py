ed14c57c246e0cf647655e6ae76098733bf1eba9
Bruksx <jenake8@gmail.com>
2026-01-19 16:01:52 +0100

    change default gender, fix welcome email

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -72,7 +72,7 @@ class User(AbstractUser, BaseModel):
     objects = CustomUserManager()
     REQUIRED_FIELDS = []
 
-    gender = models.CharField(max_length=100, choices=GenderType.choices(), default=GenderType.OTHERS.value, blank=True)
+    gender = models.CharField(max_length=100, choices=GenderType.choices(), blank=True, null=True)
     phone_number = models.CharField(max_length=50, null=True)
     phone_code = models.CharField(max_length=50, null=True)
     email = models.EmailField(unique=True, null=True)
