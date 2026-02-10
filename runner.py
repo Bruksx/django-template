117e5a356d12631246aaa69abad84a8e5e6bf0e1
Louis Ohaegbu <uzochukwu.ohaegbu.197108@unn.edu.ng>
2026-02-10 11:37:16 +0100

    add talent completion report v3

--- runner.py
+++ runner.py
@@ -5,7 +5,3 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
 
-from accounts.tasks import send_weekly_report_of_candidates
-
-send_weekly_report_of_candidates()
-
