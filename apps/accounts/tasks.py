738930fa66974d29370d54d9cde2146a6a3964bf
Louis Ohaegbu <uzochukwu.ohaegbu.197108@unn.edu.ng>
2026-02-10 14:09:47 +0100

    add talent completion report v4

--- apps/accounts/tasks.py
+++ apps/accounts/tasks.py
@@ -1,5 +1,8 @@
+from datetime import timedelta
+
 from accounts.models import Talent, User
 from accounts.queries import add_profile_completion_annotation
+from django.utils import timezone
 
 from helpers.email.accounts import send_incomplete_profile_reminder_email
 from helpers.email.utils import send_email
@@ -28,19 +31,28 @@ def remind_incomplete_profiles_task(batch_size=200):
 
 
 def send_weekly_report_of_candidates():
+    last_week = timezone.now() - timedelta(days=7)
     queryset = add_profile_completion_annotation(
         Talent.objects.all()
     )
-    completed = queryset.filter(complete_profile=True).count()
-    semi_completed = queryset.filter(semi_complete_profile=True).count()
-    incompleted = queryset.filter(semi_complete_profile=False).count()
+    total = queryset.count()
+    total_last_week = queryset.exclude(created_at__gt=last_week).count()
+
+
+    completed = queryset.filter(complete_profile=True, created_at__gt=last_week).count()
+    semi_completed = queryset.filter(semi_complete_profile=True, created_at__gt=last_week).count()
+    incompleted = queryset.filter(semi_complete_profile=False, created_at__gt=last_week).count()
 
     send_email(
         subject="Talent Completion Profile Weekly Report",
-        emails=["bryan@1840andco.com"],
+        emails=["bryan@1840andco.com", ],
         plain_body=f"""
-Number of talents with complete profiles:   {completed}
-Number of talents with semi complete profiles:   {semi_completed}
-Number of talents with incomplete profiles:    {incompleted}
+Total Number of Talents Today: {total} \n
+Total Number of Talents Last Week: {total_last_week} \n\n
+
+This Week's Data \n\n
+Number of talents with complete profiles:   {completed}\n
+Number of talents with semi complete profiles:   {semi_completed}\n
+Number of talents with incomplete profiles:    {incompleted}\n
         """
     )
