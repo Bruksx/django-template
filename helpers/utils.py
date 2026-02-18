632390b8e94bb3fa12a3c9e8dc22b3893969618f
Louis Ohaegbu <uzochukwu.ohaegbu.197108@unn.edu.ng>
2026-02-18 15:14:45 +0100

    fix ai job generate endpoint

--- helpers/utils.py
+++ helpers/utils.py
@@ -4,6 +4,7 @@ import os
 import random
 import re
 import string
+import sys
 import traceback
 import uuid
 from datetime import timezone, time, datetime
@@ -127,9 +128,11 @@ def html_to_pdf3(source_html):
         print(f"Error during PDF conversion: {e}")
         return None
 
-@test_env_decorator(True)
 def delete_s3_item(key):
     from boto3.session import Session
+
+    if "test" in sys.argv:
+        return True
 
     if not settings.USE_AWS_S3:
         Logger.info(msg=dict(sender="Helper Utils", title="AWS S3 DELETE Info",
