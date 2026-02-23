6969c47ba012497dedf15a7d983c6c205b3ef256
Louis Ohaegbu <uzochukwu.ohaegbu.197108@unn.edu.ng>
2026-02-23 20:18:30 +0100

    update roles

--- runner.py
+++ runner.py
@@ -8,13 +8,17 @@ from accounts.models import Department, Role
 
 
 def add_roles_to_db():
-    roles = "Software development Engineer Lead\nLead Software Engineer\nEngineer I\nEngineer II\nEngineer III\nFrontend Engineer\nBackend Engineer"
+    roles = "Software development Engineer\nLead Software Engineer\nEngineer I\nEngineer II\nEngineer III\nFrontend Engineer\nBackend Engineer"
     department = "Information Technology (IT)"
     industry = "General Industries"
 
     department = Department.objects.filter(name__iexact=department, industry__name__iexact=industry).first()
     if department:
         for role in roles.split("\n"):
-            role = role.strip().title()
-            if not Role.objects.filter(name__iexact=role, department=department).exists():
+            role = role.strip()
+            role_obj = Role.objects.filter(name__iexact=role, department=department).first()
+            if role_obj:
+                role_obj.update(name=role)
+            else:
                 Role.objects.create(name=role, department=department)
+
