903e3cbe693b67937801da1dae4b5a2b58be2d58
Louis Ohaegbu <uzochukwu.ohaegbu.197108@unn.edu.ng>
2026-03-02 21:11:35 +0100

    update

--- apps/accounts/views/talent.py
+++ apps/accounts/views/talent.py
@@ -155,7 +155,7 @@ def update_talent_profile(request, data: PatchDict[talent_schemas.UpdateTalentPr
         user_data["first_name"] = data.pop("first_name", None)
     if "last_name" in data:
         user_data["last_name"] = data.pop("last_name", None)
-    if "operating_system" in data:
+    if  data.get("operating_system"):
         data["operating_system"] = data["operating_system"].value if type(data["operating_system"]) is not str else data["operating_system"]
     if "gender" in data:
         user_data["gender"] = data.pop("gender", None)
