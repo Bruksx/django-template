0fc1496be278954744e8e170b1bae5ceea25aca9
Louis Ohaegbu <uzochukwu.ohaegbu.197108@unn.edu.ng>
2026-03-03 10:11:33 +0100

    debug workflow error v4

--- runner.py
+++ runner.py
@@ -1,10 +1,12 @@
 import os
 
 import django
+from django.db.models import Q
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 django.setup()
-from accounts.models import Department, Role
+from accounts.models import Department, Role, Business, BusinessUser
+from jobs.models import JobPost, Job
 
 
 def add_roles_to_db():
@@ -22,3 +24,10 @@ def add_roles_to_db():
             else:
                 Role.objects.create(name=role, department=department)
 
+def updated_job_and_job_post_created_by():
+    print(Job.objects.filter(created_by__isnull=True).count(), "jobs created_by are still null")
+    for business in Business.objects.all():
+        random_business_user = BusinessUser.objects.filter(business=business).order_by("?").first()
+        job_ids = JobPost.objects.filter(Q(recruiter__business=business)|Q(posted_by__business=business)).only("job_id").values_list("job_id", flat=True)
+        Job.objects.filter(id__in=job_ids, created_by__isnull=True).update(created_by=random_business_user)
+    print(Job.objects.filter(created_by__isnull=True).count(), "jobs created_by are still null")
