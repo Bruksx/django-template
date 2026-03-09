e16a1841cbfda83126df378080c7c70ee331d041
Louis Ohaegbu <uzochukwu.ohaegbu.197108@unn.edu.ng>
2026-03-09 14:11:50 +0100

    add description to experience

--- helpers/email/utils.py
+++ helpers/email/utils.py
@@ -3,7 +3,7 @@ from typing import List
 
 import requests
 from django.conf import settings
-from django.core.mail import EmailMultiAlternatives
+from django.core.mail import EmailMultiAlternatives, get_connection
 from django.template import loader
 from django.template.loader import render_to_string
 from django.utils.html import strip_tags
@@ -12,82 +12,143 @@ from pydantic import EmailStr
 from helpers.loggers import Logger
 from helpers.utils import is_valid_email
 
+BATCH_SIZE = 1
 
-def batch_send(func, **kwargs):
-    emails = kwargs.pop("emails", list())
-    cc = kwargs.pop("cc", list())
-    bcc = kwargs.pop("bcc", list())
-    number_of_addresses = len(emails)
-    number_of_cc = len(cc)
-    number_of_bcc = len(bcc)
-    batches = number_of_addresses // 40
-    batches_cc = number_of_cc // 40
-    batches_bcc = number_of_bcc // 40
-    remainder = number_of_addresses % 40
-    remainder_cc = number_of_cc % 40
-    remainder_bcc = number_of_bcc % 40
 
-    for batch in range(batches):
-        if cc:
-            kwargs["cc"] = cc[batch * 40: (batch + 1)* 40]
-        if bcc:
-            kwargs["bcc"] = bcc[batch * 40: (batch + 1) * 40]
+def _chunk_list(data, size):
+    for i in range(0, len(data), size):
+        yield data[i:i + size]
 
-        func(emails=emails[batch * 40: (batch + 1) * 40], **kwargs)
-    if remainder:
-        if cc:
-            kwargs["cc"] = cc[-remainder_cc:]
-        if bcc:
-            kwargs["bcc"] = bcc[-remainder_bcc:]
-        func(emails=emails[-remainder:], **kwargs)
-    if batches_cc > batches:
-        rem_batch = batches_cc - batches
-        for batch in range(rem_batch):
-            if cc:
-                kwargs["cc"] = cc[batches+batch * 40: (batches+batch + 1) * 40]
-            func(emails=[], **kwargs)
-    if batches_bcc > batches:
-        rem_batch = batches_bcc - batches
-        for batch in range(rem_batch):
+
+def _batch_send(func, *, emails=None, cc=None, bcc=None, **kwargs):
+    connection = get_connection()
+    connection.open()
+
+    emails = emails or []
+    cc = cc or []
+    bcc = bcc or []
+
+
+    email_batches = list(_chunk_list(emails, BATCH_SIZE)) or []
+    cc_batches = list(_chunk_list(cc, BATCH_SIZE)) or []
+    bcc_batches = list(_chunk_list(bcc, BATCH_SIZE)) or []
+
+    max_batches = max(len(email_batches), len(cc_batches), len(bcc_batches))
+    try:
+        for i in range(max_batches):
+            _emails = email_batches[i] if i < len(email_batches) else []
+            _cc = cc_batches[i] if i < len(cc_batches) else []
+            _bcc = bcc_batches[i] if i < len(bcc_batches) else []
+            if _cc:
+                kwargs["cc"] = _cc
             if bcc:
-                kwargs["bcc"] = bcc[batches+batch * 40: (batches+batch + 1) * 40]
-            func(emails=[], **kwargs)
+                kwargs["bcc"] = _bcc
 
+            func(
+                connection=connection,
+                emails=_emails,
+                **kwargs
+            )
+    finally:
+        connection.close()
 
+def _send_email_core(
+    connection,
+    subject,
+    body,
+    from_email,
+    to=None,
+    cc=None,
+    bcc=None,
+    html_content=None,
+    attachments=None,
+    attachment_urls=None,
+    retries=3,
+):
+    to = to or []
+    cc = cc or []
+    bcc = bcc or []
 
-
-def send_actual_email(subject:str, emails:List[str], html_body:str = None, plain_body:str=None,
-               attachments:list=None, from_user:str=None,attachment_urls:List[str]=None, retries=3):
     for retry in range(retries):
         try:
-            body = plain_body or strip_tags(html_body)
             email = EmailMultiAlternatives(
                 subject=subject,
                 body=body,
-                from_email=settings.DEFAULT_FROM_EMAIL if not from_user else f"{from_user} <{settings.EMAIL_HOST_USER}>",
-                bcc=emails,
+                from_email=from_email,
+                to=to,
+                cc=cc,
+                bcc=bcc,
+                connection=connection
             )
-            if html_body:
-                email.attach_alternative(html_body, "text/html")
+
+            if html_content:
+                email.attach_alternative(html_content, "text/html")
+
             if attachments:
                 for attachment in attachments:
-                    email.attach(attachment.name, attachment.read(), attachment.content_type)
+                    email.attach(
+                        attachment.name,
+                        attachment.read(),
+                        attachment.content_type
+                    )
+
             if attachment_urls:
                 for url in attachment_urls:
                     response = requests.get(url)
                     response.raise_for_status()
-                    email.attach(f"{url.split('/')[-1]}", response.content, mimetype=response.headers['Content-Type'])
+
+                    filename = url.split("/")[-1]
+                    mimetype = response.headers.get("Content-Type")
+
+                    email.attach(filename, response.content, mimetype)
+
             email.send(fail_silently=False)
             return
+
         except Exception as e:
-            print("error: ", e)
-            Logger.error(dict(
-                sender="Email Service",
-                title="Email was not successfully sent",
-                description=str(e)
-            ), exc_info=True)
+            Logger.error(
+                dict(
+                    sender="Email Service",
+                    title="Email was not successfully sent",
+                    description=str(e),
+                ),
+                exc_info=True
+            )
 
+            if retry == retries - 1:
+                raise
 
+def _send_actual_email(
+    connection,
+    subject: str,
+    emails: List[str],
+    html_body: str = None,
+    plain_body: str = None,
+    attachments: list = None,
+    from_user: str = None,
+    attachment_urls: List[str] = None,
+    retries=3,
+):
+
+    body = plain_body or strip_tags(html_body)
+
+    from_email = (
+        settings.DEFAULT_FROM_EMAIL
+        if not from_user
+        else f"{from_user} <{settings.EMAIL_HOST_USER}>"
+    )
+
+    _send_email_core(
+        connection=connection,
+        subject=subject,
+        body=body,
+        from_email=from_email,
+        bcc=emails,
+        html_content=html_body,
+        attachments=attachments,
+        attachment_urls=attachment_urls,
+        retries=retries,
+    )
 def send_email(subject:str, emails:List[EmailStr], html_body:str = None, plain_body:str=None,
                attachments:list=None, from_user:str=None, attachment_urls:List[str]=None):
     if "test" in sys.argv:
@@ -111,45 +172,40 @@ def send_email(subject:str, emails:List[EmailStr], html_body:str = None, plain_b
             else:
                 from_user = f"{user} from {company}"
 
-    batch_send(send_actual_email, subject=subject, emails=emails, html_body=html_body, plain_body=plain_body,
-               attachments=attachments, attachment_urls=attachment_urls,from_user=from_user)
+    _batch_send(_send_actual_email, subject=subject, emails=emails, html_body=html_body, plain_body=plain_body,
+                attachments=attachments, attachment_urls=attachment_urls, from_user=from_user)
 
 
 
-def send_actual_template_email(subject:str, body:str, emails:List[str], from_user:str, attachment_urls:List[str]=None,
-                        bcc:List[str]=None, cc:List[str]=None, html_content:str=None, attachments:list=None, retries=3):
-    for retry in range(retries):
-        try:
-            email = EmailMultiAlternatives(
-                subject=subject,
-                body=body,
-                from_email=f"{from_user} <{settings.EMAIL_HOST_USER}>",
-                to=emails
-            )
-            if bcc:
-                email.bcc = bcc
-            if cc:
-                email.cc = cc
-            if attachment_urls:
-                for url in attachment_urls:
-                    response = requests.get(url)
-                    response.raise_for_status()
-                    email.attach(f"{url.split('/')[-1]}", response.content, mimetype=response.headers['Content-Type'])
+def _send_actual_template_email(
+    connection,
+    subject: str,
+    body: str,
+    emails: List[str],
+    from_user: str,
+    attachment_urls: List[str] = None,
+    bcc: List[str] = None,
+    cc: List[str] = None,
+    html_content: str = None,
+    attachments: list = None,
+    retries=3,
+):
 
-            if attachments:
-                for attachment in attachments:
-                    email.attach(attachment.name, attachment.read(), attachment.content_type)
-            email.attach_alternative(html_content, "text/html")
-            email.send(fail_silently=False)
-            return
-        except Exception as e:
-            Logger.error(dict(
-                sender="Email Service",
-                title="Email Template was not successfully sent",
-                description=str(e)
-            ), exc_info=True)
+    from_email = f"{from_user} <{settings.EMAIL_HOST_USER}>"
 
-
+    _send_email_core(
+        connection=connection,
+        subject=subject,
+        body=body,
+        from_email=from_email,
+        to=emails,
+        cc=cc,
+        bcc=bcc,
+        html_content=html_content,
+        attachments=attachments,
+        attachment_urls=attachment_urls,
+        retries=retries,
+    )
 
 def send_template_email(subject:str, body:str, emails:List[str], from_user:str, attachment_urls:List[str]=None,
                         bcc:List[str]=None, cc:List[str]=None, html_content:str=None, attachments:list=None):
@@ -177,8 +233,8 @@ def send_template_email(subject:str, body:str, emails:List[str], from_user:str, 
         else:
             from_user = f"{user} from {company}"
 
-    batch_send(send_actual_template_email, subject=subject, body=body, emails=emails, from_user=from_user, attachment_urls=attachment_urls,
-               bcc=bcc, cc=cc, html_content=html_content, attachments=attachments)
+    _batch_send(_send_actual_template_email, subject=subject, body=body, emails=emails, from_user=from_user, attachment_urls=attachment_urls,
+                bcc=bcc, cc=cc, html_content=html_content, attachments=attachments)
 
 
 
