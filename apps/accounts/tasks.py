2b0728fd98cf6091c72c9acef952f091d45b07b8
Bruksx <jenake8@gmail.com>
2026-03-11 22:33:42 +0100

    pause incomplete profile mail

--- apps/accounts/tasks.py
+++ apps/accounts/tasks.py
@@ -22,4 +22,4 @@ def remind_incomplete_profiles_task(batch_size=200):
             .values_list("email", flat=True)
         )
 
-        send_incomplete_profile_reminder_email(emails=emails)
+        # send_incomplete_profile_reminder_email(emails=emails)
