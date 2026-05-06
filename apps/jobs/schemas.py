4b1ae156e5b724862aa13dd624b0ce47f0556c86
Louis Ohaegbu <uzochukwu.ohaegbu.197108@unn.edu.ng>
2026-05-06 14:39:25 +0100

    update job

--- apps/jobs/schemas.py
+++ apps/jobs/schemas.py
@@ -577,7 +577,7 @@ class JobDetailSchema(ModelSchema):
             "hiring_company_name", "hiring_company_description", "about", "years_of_experience",
             "technological_requirement", "work_structure", "office_address","lunch_break", "lunch_break_time",
             "additional_hours_start", "additional_hours_end", "flexible_availability", "qualification",
-            "availability_timezone", "additional_hours_description", "additional_skills" "cv_required"
+            "availability_timezone", "additional_hours_description", "additional_skills", "cv_required"
         ]
     
     @staticmethod
