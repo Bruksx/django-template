051898c27fc8cef497ec092eb48dd3d8236aee88
Louis Ohaegbu <uzochukwu.ohaegbu.197108@unn.edu.ng>
2026-06-15 18:11:36 +0100

    add business xml link

--- apps/jobs/external_views.py
+++ apps/jobs/external_views.py
@@ -1,6 +1,8 @@
+from uuid import UUID
+
 from django.http import StreamingHttpResponse
 from ninja import Router
-
+from ninja.errors import HttpError
 from services.job_posting.linkedin_xml_generator import generate_job_post_xml_stream
 from services.job_posting.schema.indeed_xml import Source
 
@@ -27,4 +29,23 @@ def get_indeed_job_posts_xml_pool(request, *args, **kwargs):
     page_size = request.GET.get("page-size")
     return StreamingHttpResponse(Source.to_xml_stream(page, page_size), content_type="application/xml")
 
+@router.get("business/{uid}/job-posts/indeed-pool.xml", summary="fetch job posts for linkedin in xml format",
+            tags=["Job Posts"])
+def get_indeed_business_job_posts_xml_pool(request, uid: UUID, *args, **kwargs):
+    from accounts.models import Business
+    page = request.GET.get("page")
+    page_size = request.GET.get("page-size")
+    business = Business.objects.filter(uid=uid).first()
+    if not business:
+        raise HttpError(404, "Business not found")
+    return StreamingHttpResponse(Source.to_xml_stream(page, page_size, business.id), content_type="application/xml")
 
+
+@router.get("business/{uid}/job-posts/linkedin-pool.xml", summary="fetch job posts for linkedin in xml format",
+            tags=["Job Posts"])
+def get_linkedin_business_job_posts_xml_pool(request, uid: UUID, *args, **kwargs):
+    from accounts.models import Business
+    business = Business.objects.filter(uid=uid).first()
+    if not business:
+        raise HttpError(404, "Business not found")
+    return StreamingHttpResponse(generate_job_post_xml_stream(business_id=business.id), content_type="application/xml")
