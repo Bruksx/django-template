bd3c3116ccd3c94189194bb6f0624de077b88d8f
Bruksx <jenake8@gmail.com>
2026-07-08 15:06:15 +0100

    update rank query

--- apps/jobs/business_views.py
+++ apps/jobs/business_views.py
@@ -7,7 +7,7 @@ from ai.models import AIMatchScore
 from config.permissions import IsBusinessUser
 from django.db import transaction
 from django.db.models import Q, Exists, OuterRef, Case, When, F, Value, Subquery, Window, Count
-from django.db.models.functions import Concat, DenseRank
+from django.db.models.functions import Concat, DenseRank, RowNumber
 from django.shortcuts import get_object_or_404
 from django.utils import timezone
 from helpers.email.jobs import send_indeed_apply_email
@@ -597,7 +597,7 @@ def view_applicant(request, application_uid: UUID):
         ai_match_score=Subquery(score_subquery)
     ).annotate(
         rank=Window(
-            expression=DenseRank(),
+            expression=RowNumber(),
             partition_by=[F("job_post_id")],
             order_by=F("ai_match_score").desc(),
         )
@@ -642,7 +642,7 @@ def view_applicant_detail(request, application_uid: UUID):
                 ai_match_score=Subquery(score_subquery)
             ).annotate(
                 rank=Window(
-                    expression=DenseRank(),
+                    expression=RowNumber(),
                     partition_by=[F("job_post_id")],
                     order_by=F("ai_match_score").desc(),
                 )
@@ -696,7 +696,7 @@ def view_applicants(request, job_post_uid:UUID, page_size=50, page=1, phase:Opti
                 ai_match_score=Subquery(score_subquery)
             ).annotate(
                 rank=Window(
-                    expression=DenseRank(),
+                    expression=RowNumber(),
                     partition_by=[F("job_post_id")],
                     order_by=F("ai_match_score").desc(),
                 )
